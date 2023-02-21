785f5a22c2c2a0bc9a7b5cbc351c327917d2f8ab
Atul Aryan <imatularyan@gmail.com>
2023-02-22 03:13:14 +0530

    functional component added

--- App.js
+++ App.js
@@ -1,31 +1,34 @@
-import React from 'react';
 import ReactDOM from "react-dom/client";
 
-// JSX => React.createElement => Object => HTML(DOM);
+// Functional components
 
-const heading = (
-    <h1 className="title">Heading</h1>
-);
+// It won't work because It is a function
+const Heading = () => {
+    return <h1 className='title'>Heading</h1>
+};
 
-const heading2 = (
-    <h2 className="title">Heading2</h2>
-);
+const Heading2 = () => {
+    return <h2 className='title'>Heading 2</h2>
+};
 
-const heading3 = (
-    <h3 className="title">Heading3</h3>
-);
+const Heading3 = () => {
+    return <h3 className='title'>Heading 3</h3>
 
-const container = (
-    <div>
-        {heading}
-        {heading2}
-        {heading3}
+};
+
+const Container = () => {
+    return (
+    <div className='container'>
+        {Heading}
+        {<Heading2/>}
+        {<Heading3></Heading3>}
     </div>
-);
+    )
+};
 
 // create root element using createRoot 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 // passing react element inside root
 // async defer
-root.render(container); 
+root.render(<Container/>); 
