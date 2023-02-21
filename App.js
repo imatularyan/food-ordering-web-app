1b1805a43fe5f8e78febbf9a01e6c4be9586360a
Atul Aryan <imatularyan@gmail.com>
2023-02-22 02:27:07 +0530

    Nested header element using React.createElement

--- App.js
+++ App.js
@@ -1,48 +1,32 @@
-/**
- * 
- * Create Server
- * Hot Module Replacement
- * File Watcher algorithm - c++
- * BUNDLING
- * MINIFY
- * Cleaning our Code
- * Dev and Production Build
- * Super Fast build algorithm
- * Image Optimization
- * Caching while development
- * Compression
- * Compatable with older version of browsers
- * HTTPS on dev
- * Port Number
- * Consistent Hashing Algorithm
- * Zero Configuration
- * 
- * 
- * Transitive Dependencies
- * 
- */
-
-
-import React from "react";
+import React from 'react';
 import ReactDOM from "react-dom/client";
 
-// create nested react elements
-const heading = React.createElement('h1', {
-    id: 'title'
-}, "Hello World");
-const heading2 = React.createElement('h2', {
-    id: 'title2'
-}, "Hello World 2");
-const container = React.createElement('div', {
-    id: 'container',
-}, [heading, heading2]);
+// React.createElement => Object => HTML(DOM)
+// Nested header element using React.createElement
 
-console.log(heading);
+const heading = React.createElement("h1",
+{
+    className: "title"
+}, "Heading");
+
+const heading2 = React.createElement("h2",
+{
+    className: "title",
+}, "Heading 2");
+
+const heading3 = React.createElement("h3",
+{
+    className: "title",
+}, "Heading 3");
+
+const container = React.createElement("div",
+{
+    className: "container",
+}, [heading, heading2, heading3]);
 
 // create root element using createRoot 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 // passing react element inside root
-
 // async defer
 root.render(container); 
