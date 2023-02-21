708b8400945b5d0f0896d7a220576d5ff9c88ee6
Atul Aryan <imatularyan@gmail.com>
2023-02-22 02:42:03 +0530

    chapter-03 Laying the foundation, same element using JSX

--- App.js
+++ App.js
@@ -1,28 +1,27 @@
 import React from 'react';
 import ReactDOM from "react-dom/client";
 
-// React.createElement => Object => HTML(DOM)
-// Nested header element using React.createElement
+// JSX => React.createElement => Object => HTML(DOM);
 
-const heading = React.createElement("h1",
-{
-    className: "title"
-}, "Heading");
+const heading = (
+    <h1 className="title">Heading</h1>
+);
 
-const heading2 = React.createElement("h2",
-{
-    className: "title",
-}, "Heading 2");
+const heading2 = (
+    <h2 className="title">Heading2</h2>
+);
 
-const heading3 = React.createElement("h3",
-{
-    className: "title",
-}, "Heading 3");
+const heading3 = (
+    <h3 className="title">Heading3</h3>
+);
 
-const container = React.createElement("div",
-{
-    className: "container",
-}, [heading, heading2, heading3]);
+const container = (
+    <div>
+        {heading}
+        {heading2}
+        {heading3}
+    </div>
+);
 
 // create root element using createRoot 
 const root = ReactDOM.createRoot(document.getElementById('root'));
