a77a082277ef31e0f198987f5793f57c23ee6f4b
Atul Aryan <imatularyan@gmail.com>
2023-02-22 23:59:43 +0530

    css optimized

--- App.js
+++ App.js
@@ -2,13 +2,16 @@ import ReactDOM from "react-dom/client";
 import Logo from "./assets/logo.png";
 import User from "./assets/user.svg";
 
+// React element
+const title = (
+    <h1 className="logo"><img src={Logo}/></h1>
+);
+
 // functional header component
 const HeaderComponent = () => {
     return (
         <div className="header">
-        <div className="logo">
-            <img src={Logo}/>
-        </div>
+        {title}
         <div className="search-field">
             <input type="text" className="search-input"></input>
             <button type="search" className="search-button">Search</button>
